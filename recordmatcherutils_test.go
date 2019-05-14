3cbe85a7fbdd0004c9915a01a1bc1b5d279c1665
Simon Tucker <simon@brotherlogic@gmail>
2019-05-14 16:01:11 -0700

    Adds processing of matches.,

--- recordmatcherutils_test.go
+++ recordmatcherutils_test.go
@@ -7,19 +7,20 @@ import (
 	"github.com/brotherlogic/keystore/client"
 	"golang.org/x/net/context"
 
+	pbgd "github.com/brotherlogic/godiscogs"
 	pbrc "github.com/brotherlogic/recordcollection/proto"
 )
 
 type testGetter struct {
 	fail bool
-	rec  *pbrc.Record
+	rec  []*pbrc.Record
 }
 
 func (t *testGetter) getRecords(ctx context.Context) ([]*pbrc.Record, error) {
 	if t.fail {
 		return nil, fmt.Errorf("Build to fail")
 	}
-	return []*pbrc.Record{t.rec}, nil
+	return t.rec, nil
 }
 
 func (t *testGetter) update(ctx context.Context, r *pbrc.Record) error {
@@ -35,8 +36,17 @@ func InitTest() *Server {
 	return s
 }
 
+func TestVeryBasicTest(t *testing.T) {
+	s := InitTest()
+	err := s.processRecords(context.Background())
+	if err != nil {
+		t.Errorf("Failed: %v", err)
+	}
+}
+
 func TestBasicTest(t *testing.T) {
 	s := InitTest()
+	s.getter = &testGetter{rec: []*pbrc.Record{&pbrc.Record{Release: &pbgd.Release{MasterId: 123}}, &pbrc.Record{Release: &pbgd.Release{MasterId: 123}}}}
 	err := s.processRecords(context.Background())
 	if err != nil {
 		t.Errorf("Failed: %v", err)
