6f113a82c7ee679481d932646677ecfcafd301dd
Simon Tucker <brotherlogic@gmail.com>
2020-04-09 15:56:52 -0700

    Add api

---
+++ recordmatcherapi.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/recordmatcher/proto"
+)
+
+// Match - force matches a record
+func (s *Server) Match(ctx context.Context, req *pb.MatchRequest) (*pb.MatchResponse, error) {
+	return &pb.MatchResponse{}, s.processRecordList(ctx, []int32{req.GetInstanceId()})
+}
